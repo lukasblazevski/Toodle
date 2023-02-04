d3311d475b198852fb0807092d9f31f0098a2947
lukasblazevski <lukasblazevski@gmail.com>
2023-02-04 17:26:05 -0500

    removed hello world, renamed login page to index

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,44 @@
-export default function Component() {
-	return (
-		<>
-			Hello world!
-		</>
-	);
-}
+import { Field, Form, Formik } from 'formik';
+import Link from 'next/link';
+import api from '../lib/api';
+
+const Component = () => (
+	<>
+
+	<div class="login-container">
+		<h2>Log in to Toodle</h2>
+
+		<Formik
+			initialValues={{
+				username: '',
+				password: '',
+			}}
+			onSubmit={async ({ username, password}) => {
+				
+
+				await api.post('/users', { username, password });
+
+				location.href = '/user';
+			}}
+		>
+			<Form>
+				<label htmlFor="username">Username: </label>
+				<Field type="text" name="username" id="username" required autoFocus autoComplete="off" />
+				<br />
+				<br />
+				<label htmlFor="password">Password: </label>
+				<Field type="password" name="password" id="password" required />
+				<br />
+				<br />
+				
+
+				<div>
+					<button type="submit" class="login-button">Log In</button> 
+				</div>
+			</Form>
+		</Formik>
+		</div>
+	</>
+);
+
+export default Component;
