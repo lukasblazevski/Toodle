5e0e2fa04cac8bd6b1032dc616d42021211cb890
Grant Gryczan <grantgryczan@gmail.com>
2023-02-06 18:40:10 -0500

    More responsive with flexboxes

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,36 +5,38 @@ import api from '../lib/api';
 export default function Component() {
 	return (
 		<div className="login-container">
-			<h2>Log in to Toodle</h2>
+			<div className="login-box">
+				<h2>Log in to Toodle</h2>
 
-			<Formik
-				initialValues={{
-					username: '',
-					password: '',
-				}}
-				onSubmit={async ({ username, password}) => {
-					await api.post('/users', { username, password });
+				<Formik
+					initialValues={{
+						username: '',
+						password: '',
+					}}
+					onSubmit={async ({ username, password}) => {
+						await api.post('/users', { username, password });
 
-					location.href = '/user';
-				}}
-			>
-				<Form>
-					<Field type="text" name="username" id="username" className="up-format" placeholder="Username or Email" required autoFocus />
-					<br />
-					<br />
-					<Field type="password" name="password" id="password" className="up-format" placeholder="Password" required />
-					<br />
-					<a href="#" className="link">
-						Forgot Your Password?
-					</a>
-					<br />
-					<br />
+						location.href = '/user';
+					}}
+				>
+					<Form>
+						<Field type="text" name="username" id="username" className="up-format" placeholder="Username or Email" required autoFocus />
+						<br />
+						<br />
+						<Field type="password" name="password" id="password" className="up-format" placeholder="Password" required />
+						<br />
+						<a href="#" className="link">
+							Forgot Your Password?
+						</a>
+						<br />
+						<br />
 
-					<div>
-						<button type="submit" id="login-button">Log In</button>
-					</div>
-				</Form>
-			</Formik>
+						<div>
+							<button type="submit" id="login-button">Log In</button>
+						</div>
+					</Form>
+				</Formik>
+			</div>
 		</div>
 	);
 }
