a4fe965545a856326fbe3f5997926671ced8bd17
Grant Gryczan <grantgryczan@gmail.com>
2023-02-06 18:18:58 -0500

    Nav component, formatting updates

---
+++ components/Nav.jsx
@@ -0,0 +1,40 @@
+export default function Nav() {
+	return (
+		<ul className="navbar">
+			<a><img id="navbar-toodle-logo" src="favicon.ico"/> </a>
+			<li className="navbar-li">
+				<div className="dropdown">
+					<button className="dropbtn" id="first-drop-btn">Current Courses <span>&#62;</span> </button>
+					<div className="dropdown-content">
+						<a href="#">CSI-4999-Capstone</a>
+						<a href="#">CSI-4160-Integrated Computing Systems</a>
+						<a href="#">CSI-4480-Information Security</a>
+					</div>
+				</div>
+			</li>
+			<li className="navbar-li">
+				<div className="dropdown">
+					<button className="dropbtn" onclick="window.location.href='https://mysail.oakland.edu';">MySail</button>
+				</div>
+			</li>
+			<li className="navbar-li">
+				<div className="dropdown">
+					<button className="dropbtn" onclick="window.location.href='https://www2.oakland.edu/secure/esp/';">Get Help</button>
+				</div>
+			</li>
+
+			<li className="navbar-li-right">
+				<div className="dropdown">
+					<a><img className="dropdown" id="nav-profile-photo" src="default_user_profile.svg"/> </a>
+					<div className="dropdown-content">
+					<div className="dropdown-content-right">
+					<a>Preferences</a>
+					<a>Logout</a>
+					</div>
+				</div>
+				</div>
+				<a href="https://google.com"><img id="OU-logo" src="OU_Dark.svg"/></a>
+			</li>
+		</ul>
+	);
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,45 +2,39 @@ import { Field, Form, Formik } from 'formik';
 import Link from 'next/link';
 import api from '../lib/api';
 
-const Component = () => (
-	<>
+export default function Component() {
+	return (
+		<div className="login-container">
+			<h2>Log in to Toodle</h2>
 
-	<div className="login-container">
-		<h2>Log in to Toodle</h2>
+			<Formik
+				initialValues={{
+					username: '',
+					password: '',
+				}}
+				onSubmit={async ({ username, password}) => {
+					await api.post('/users', { username, password });
 
-		<Formik
-			initialValues={{
-				username: '',
-				password: '',
-			}}
-			onSubmit={async ({ username, password}) => {
-				
+					location.href = '/user';
+				}}
+			>
+				<Form>
+					<Field type="text" name="username" id="username" className="up-format" placeholder="Username or Email" required autoFocus />
+					<br />
+					<br />
+					<Field type="password" name="password" id="password" className="up-format" placeholder="Password" required />
+					<br />
+					<a href="#" className="link">
+						Forgot Your Password?
+					</a>
+					<br />
+					<br />
 
-				await api.post('/users', { username, password });
-
-				location.href = '/user';
-			}}
-		>
-			<Form>
-				<Field type="text" name="username" id="username" className="up-format" placeholder="Username or Email" required autoFocus autoComplete="off" />
-				<br />
-				<br />
-				<Field type="password" name="password" id="password" className="up-format" placeholder="Password" required />
-				<br />
-				<a href="#" className="link">
-  					Forgot Your Password?
-				</a>
-				<br />
-				<br />
-				
-
-				<div>
-					<button type="submit" id="login-button">Log In</button> 
-				</div>
-			</Form>
-		</Formik>
+					<div>
+						<button type="submit" id="login-button">Log In</button>
+					</div>
+				</Form>
+			</Formik>
 		</div>
-	</>
-);
-
-export default Component;
+	);
+}
