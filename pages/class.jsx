ecf46aae9652a1bdd1bde924d06801e44becdb5b
lukasblazevski <lukasblazevski@gmail.com>
2023-02-07 22:36:02 -0500

    started class page

---
+++ pages/class.jsx
@@ -0,0 +1,15 @@
+import { Field, Form, Formik } from 'formik';
+import Link from 'next/link';
+import api from '../lib/api';
+import React from "react";
+import {Nav} from "react-bootstrap";
+import { withRouter } from "react-router";
+
+
+export default function Component() {
+	return (
+		<>
+		
+		</>
+	);
+}
